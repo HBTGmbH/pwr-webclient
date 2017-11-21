e37bef16ccd27bcffef82a072690dbd6f77f2c54
chb <chb@hbt.de>
2017-11-21 11:09:32 +0100

    paths mit führendem /power/

--- public/config.js
+++ public/config.js
@@ -1,47 +1,21 @@
+const POWER_API_HOST = "https://power.hbt.de";
+const POWER_API_PORT = "";
+const POWER_API_SUFFIX_PROFILE = "/power/api/pwr-profile-service/api";
+const POWER_PROFILE_META_INFO = POWER_API_HOST + ":" + POWER_API_PORT + "/power/api/pwr-profile-service/meta";
 
-/*
-const POWER_API_HOST = "http://localhost";
-const POWER_API_PORT = "9004";
-const POWER_API_SUFFIX_PROFILE = "/api";
-const POWER_PROFILE_META_INFO = POWER_API_HOST + ":" + POWER_API_PORT + "/meta";
-/*/
-const POWER_API_HOST = "http://power02.corp.hbt.de";
-const POWER_API_PORT = "9000";
-const POWER_API_SUFFIX_PROFILE = "/pwr-profile-service/api";
-const POWER_PROFILE_META_INFO = POWER_API_HOST + ":" + POWER_API_PORT + "/pwr-profile-service/meta";
-//*/
+const POWER_API_HOST_STATISTICS = "https://power.hbt.de";
+const POWER_API_PORT_STATISTICS = "";
+const POWER_API_SUFFIX_STATISTICS = "/power/api/pwr-statistics-service";
 
-//*/
-const POWER_API_HOST_STATISTICS = "http://power02.corp.hbt.de";
-const POWER_API_PORT_STATISTICS = "9000";
-const POWER_API_SUFFIX_STATISTICS = "/pwr-statistics-service";
-/*/
-const POWER_API_HOST_STATISTICS = "http://localhost";
-const POWER_API_PORT_STATISTICS = "9007";
-const POWER_API_SUFFIX_STATISTICS = "";
-//*/
+const POWER_API_HOST_SKILL = "https://power.hbt.de";
+const POWER_API_PORT_SKILL = "";
+const POWER_API_SUFFIX_SKILL = "/power/api/pwr-skill-service";
 
-//*/
-const POWER_API_HOST_SKILL = "https://power02.corp.hbt.de";
-const POWER_API_PORT_SKILL = "9000";
-const POWER_API_SUFFIX_SKILL = "/pwr-skill-service";
-/*/
-const POWER_API_HOST_SKILL = "http://localhost";
-const POWER_API_PORT_SKILL = "9003";
-const POWER_API_SUFFIX_SKILL = "";
-//*/
+const POWER_API_HOST_VIEW = "https://power.hbt.de";
+const POWER_API_PORT_VIEW = "";
+const POWER_API_SUFFIX_VIEW = "/power/api/pwr-view-profile-service";
 
-//*/
-const POWER_API_HOST_VIEW = "http://power02.corp.hbt.de";
-const POWER_API_PORT_VIEW = "9000";
-const POWER_API_SUFFIX_VIEW = "/pwr-view-profile-service";
-/*/
-const POWER_API_HOST_VIEW = "http://localhost";
-const POWER_API_PORT_VIEW = "9008";
-const POWER_API_SUFFIX_VIEW = "";
-//*/
-
-const POWER_API_META_INFO_REPORT = "http://power02.corp.hbt.de:9000/pwr-report-service/meta";
+const POWER_API_META_INFO_REPORT = "https://power.hbt.de/power/api/pwr-report-service/meta";
 
 const GLOBAL_OPTIONS = {
     defaultCharsPerLine: 32,
