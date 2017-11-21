46a138076fc3cca6013023f3199145f9eedc4aee
chb <chb@hbt.de>
2017-11-21 12:40:14 +0100

    paths mit führendem /power/

--- src/Paths.ts
+++ src/Paths.ts
@@ -16,30 +16,30 @@ import {NavigationActionCreator} from './reducers/navigation/NavigationActionCre
  * Paths used for routing. Central point of configuration for routing information.
  */
 export class Paths {
-    public static readonly APP_ROOT = '/power';
-    public static readonly ADMIN_LOGIN = '/power/login';
+    public static readonly APP_ROOT = '/';
+    public static readonly ADMIN_LOGIN = '/login';
 
 
-    public static readonly ADMIN_BASE = '/power/admin/';
-    public static readonly ADMIN_INBOX = '/power/admin/home/inbox';
-    public static readonly ADMIN_TRASHBOX = '/power/admin/home/trashbox';
-    public static readonly ADMIN_CONSULTANTS = '/power/admin/home/consultants';
-    public static readonly ADMIN_STATISTICS_SKILL = '/power/admin/home/statistics/skills';
-    public static readonly ADMIN_STATISTICS_NETWORK = '/power/admin/home/statistics/network';
-    public static readonly ADMIN_INFO_SKILLTREE = '/power/admin/home/info/skilltree';
-    public static readonly ADMIN_INFO_NAME_ENTITY = '/power/admin/home/info/names';
+    public static readonly ADMIN_BASE = '/admin/';
+    public static readonly ADMIN_INBOX = '/admin/home/inbox';
+    public static readonly ADMIN_TRASHBOX = '/admin/home/trashbox';
+    public static readonly ADMIN_CONSULTANTS = '/admin/home/consultants';
+    public static readonly ADMIN_STATISTICS_SKILL = '/admin/home/statistics/skills';
+    public static readonly ADMIN_STATISTICS_NETWORK = '/admin/home/statistics/network';
+    public static readonly ADMIN_INFO_SKILLTREE = '/admin/home/info/skilltree';
+    public static readonly ADMIN_INFO_NAME_ENTITY = '/admin/home/info/names';
 
     public static readonly USER_SPECIAL_LOGOUT = '##LOGOUT_USER##';
 
-    public static readonly USER_BASE = '/power/app/';
-    public static readonly USER_HOME = '/power/app/home';
-    public static readonly USER_VIEW_PROFILE = '/power/app/view/:id';
-    public static readonly USER_PROFILE = '/power/app/profile';
-    public static readonly USER_REPORTS = '/power/app/reports';
-    public static readonly USER_SEARCH = '/power/app/search';
-    public static readonly USER_STATISTICS_NETWORK =  '/power/app/statistics/network';
-    public static readonly USER_STATISTICS_CLUSTERINFO = '/power/app/statistics/clusterinfo';
-    public static readonly USER_STATISTICS_SKILLS = '/power/app/statistics/skills';
+    public static readonly USER_BASE = '/app/';
+    public static readonly USER_HOME = '/app/home';
+    public static readonly USER_VIEW_PROFILE = '/app/view/:id';
+    public static readonly USER_PROFILE = '/app/profile';
+    public static readonly USER_REPORTS = '/app/reports';
+    public static readonly USER_SEARCH = '/app/search';
+    public static readonly USER_STATISTICS_NETWORK =  '/app/statistics/network';
+    public static readonly USER_STATISTICS_CLUSTERINFO = '/app/statistics/clusterinfo';
+    public static readonly USER_STATISTICS_SKILLS = '/app/statistics/skills';
 
     constructor() {
 
