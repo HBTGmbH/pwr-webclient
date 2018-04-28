a6a83d803970a7571a802c1fa98d39de2d69a816
nt <nt@hbt.de>
2018-04-28 10:32:35 +0200

    [general] Added version utility scripts

---
+++ scripts/increment-release-version.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+let version = packageJson.version.split(".");
+let majorVersion = Number.parseInt(version[0]);
+let minorVersion = Number.parseInt(version[1]);
+let incrementalVersion = Number.parseInt(version[2]);
+
+let newVersion = majorVersion + "." + (minorVersion + 1) + ".0";
+packageJson.version = newVersion;
+
+fs.writeFileSync('./package.json', JSON.stringify(packageJson, 2, 2), 'utf8');
